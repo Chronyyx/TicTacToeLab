daeaa40c87746fb6b6071ca2021208a0977b6553
Chronyx <71571496+Chronyyx@users.noreply.github.com>
2024-11-17 21:49:43 -0500

    Add project files.

---
+++ TicTacToe/Board.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    public class Board
+    {
+        private Player[,] grid;
+        public Player CurrentPlayer
+        {
+            get;
+            private set;
+        }
+
+        public Board()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            grid = new Player[3, 3];
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    grid[row, col] = Player.None;
+                }
+            }
+            CurrentPlayer = Player.X;
+        }
+
+        public bool Select(int row, int column)
+        {
+            if (grid[row, column] == Player.None)
+            {
+                grid[row, column] = CurrentPlayer;
+                CurrentPlayer = CurrentPlayer == Player.X ? Player.O : Player.X;
+                return true;
+            }
+            return false;
+        }
+
+        public Player CheckWin()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (grid[i, 0] != Player.None && grid[i, 0] == grid[i, 1] && grid[i, 1] == grid[i, 2])
+                    return grid[i, 0];
+                if (grid[0, i] != Player.None && grid[0, i] == grid[1, i] && grid[1, i] == grid[2, i])
+                    return grid[0, i];
+            }
+            if (grid[0, 0] != Player.None && grid[0, 0] == grid[1, 1] && grid[1, 1] == grid[2, 2])
+                return grid[0, 0];
+            if (grid[0, 2] != Player.None && grid[0, 2] == grid[1, 1] && grid[1, 1] == grid[2, 0])
+                return grid[0, 2];
+
+            return Player.None;
+        }
+
+        public bool IsTie()
+        {
+            foreach (var cell in grid)
+            {
+                if (cell == Player.None)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace TicTacToe
+{
+    public partial class MainWindow : Window
+    {
+        private Board board;
+        private int gamesPlayed = 0;
+        private int gamesWon = 0;
+
+        private string blankImagePath = "C:/Users/aidav/source/repos/TicTacToe/TicTacToe/images/blank.png";
+        private string xImagePath = "C:/Users/aidav/source/repos/TicTacToe/TicTacToe/images/x.png";
+        private string oImagePath = "C:/Users/aidav/source/repos/TicTacToe/TicTacToe/images/o.png";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            board = new Board();
+            ResetGame();
+        }
+
+        private void Cell_Click(object sender, MouseButtonEventArgs e)
+        {
+            Image clickedCell = (Image)sender;
+            int cellIndex = int.Parse(clickedCell.Tag.ToString());
+            int row = cellIndex / 3;
+            int column = cellIndex % 3;
+
+            if (board.Select(row, column))
+            {
+
+                clickedCell.Source = new BitmapImage(new Uri(
+                    board.CurrentPlayer == Player.O ? xImagePath : oImagePath));
+
+                Player winner = board.CheckWin();
+                if (winner != Player.None)
+                {
+                    MessageBox.Show($"{winner} wins!");
+                    gamesPlayed++;
+
+                    if (winner == Player.X)
+                    {
+                        gamesWon++;
+                    }
+
+                    UpdateNotificationArea();
+                    ResetGame();
+                    return;
+                }
+
+                if (board.IsTie())
+                {
+                    MessageBox.Show("It's a tie!");
+                    gamesPlayed++;
+                    UpdateNotificationArea();
+                    ResetGame();
+                    return;
+                }
+
+                UpdateNotificationArea();
+            }
+        }
+
+        private void ResetGame()
+        {
+            board.Reset();
+            foreach (Image cell in GameGrid.Children)
+            {
+                cell.Source = new BitmapImage(new Uri(blankImagePath));
+            }
+            UpdateNotificationArea();
+        }
+
+        private void UpdateNotificationArea()
+        {
+            GamesPlayedText.Text = $"Games Played: {gamesPlayed}";
+            GamesWonText.Text = $"Games Won: {gamesWon}";
+            double winRatio = gamesPlayed == 0 ? 0 : (double)gamesWon / gamesPlayed * 100;
+            WinRatioText.Text = $"Win Ratio: {winRatio}%";
+            TurnText.Text = $"Turn: Player {board.CurrentPlayer}";
+        }
+    }
+}
